99a1f8ebf445234ce9c98137b924c335d1858412
Bauke <broesink@gmail.com>
2026-05-12 21:05:54 +0200

    This script displays a simple businesscard

---
+++ businesscard.py
@@ -0,0 +1,11 @@
+name = "Bauke"
+occupation = "Junior Python developer"
+location = "Benidorm, Spain"
+motto = "Never stop learning!"
+
+print("=" * 40)
+print(f"Name:           {name}")
+print(f"Occupation:     {occupation}")
+print(f"Location:       {location}")
+print(f"Motto:          {motto}")
+print("=" * 40)

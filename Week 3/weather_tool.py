a2e634200db989d2c0339b473c86a413f1faefce
Bauke <broesink@gmail.com>
2026-05-25 10:14:30 +0200

    Built a weather tool that asks the user for a city, then retrieves the weather for that city and saves it in a json file. The next time the last three items of the history are displayed. This exercise combines everything from this week, dictionaries, files, JSON and API calls

--- Week 3/weather_tool.py
+++ Week 3/weather_tool.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import os
+from datetime import datetime
 
 def get_coordinates(city_name):
     url = "https://geocoding-api.open-meteo.com/v1/search"
@@ -23,10 +25,45 @@ def get_weather(lat, lon):
         weather = data["current_weather"]
         return weather["temperature"]
     else:
-        print(f"Oops, something went wrong with retreiving weather info: {response.status_code}")
+        print(f"Oops, something went wrong with retrieving weather info: {response.status_code}")
 
+def save_to_history(city, temperature):
+    history = []
+    
+    if os.path.exists("history.json"):
+        with open("history.json", "r") as file:
+            history = json.load(file)
+
+    new_search = {
+        "city" : city,
+        "temperature": temperature,
+        "time": datetime.now().strftime("%Y-%m-%d %H:%M")
+    }
+
+    history.append(new_search)
+
+    with open("history.json", "w") as file:
+        json.dump(history, file, indent=4)
+
+def show_history():
+
+    history = []
+
+    if os.path.exists("history.json"):
+        with open("history.json", "r") as file:
+            history = json.load(file)
+
+    for items in history[-3:]:
+        print(f"City: {items['city']} | Temperature: {items['temperature']} | Time: {items['time']}")
+        
+
+show_history()
 city = input("Where do you live? ")
 lat, lon = get_coordinates(city)
-temperature = get_weather(lat, lon)
-print(f"The temperature in {city} is {temperature} degrees.")
 
+if lat is None:
+    print("City not found, try again.")
+else:
+    temperature = get_weather(lat, lon)
+    print(f"The temperature in {city} is {temperature} degrees.")
+    save_to_history(city, temperature)

d0fef424b08579770ab7abef95247570d1b62db3
Bauke <broesink@gmail.com>
2026-05-27 20:44:43 +0200

    Updated the helpers.py file with if __name__ == '__main__', to practice that there can be content that only loads when the file is loaded directly, when imported in another file, that code wont run. Great for testing

--- Week 4/helpers.py
+++ Week 4/helpers.py
@@ -14,3 +14,10 @@ def print_divider(char="-", length=40):
 
 APP_NAME = "SEO Keyword Analyzer"
 VERSION = "1.0"
+
+#the following will only run if the current file helpers.py runs directly
+#it wont run if this current file is imported in a different file
+if __name__ == "__main__":
+    print("Testing helpers module...")
+    print(clean_input("    Hello   World  "))
+    print(format_number(150000000000000))

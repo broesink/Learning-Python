10627d5ce8bc4d46c71d602869d4b8b0f218cf4c
Bauke <broesink@gmail.com>
2026-06-17 10:04:12 +0200

    Finished the exercise for lesson 3, week 5. Now i have one parent class, and child classes that work together with the parent class.

--- Week 5/employees.py
+++ Week 5/employees.py
@@ -11,7 +11,7 @@ class Employee:
         return self.base_salary * 12
     
     def introduce(self):
-        print(f'Welcome {self.name}. Your ID is: {self.employee_id} and your annual salary is: {self.get_annual_salary}')
+        print(f'Welcome {self.name}. Your ID is: {self.employee_id} and your annual salary is: {self.get_annual_salary()}')
 
 class Manager(Employee):
     def __init__(self, name, employee_id, base_salary, department, team_size):
@@ -29,8 +29,51 @@ class Manager(Employee):
         print(f'{self.name} is holding a meeting about {topic}')
 
 
-employee1 = Employee('James', 34234, 5000)
+class Developer(Employee):
+    def __init__(self, name, employee_id, base_salary, programming_language, seniority):
+        super().__init__(name, employee_id, base_salary)
+        self.programming_language = programming_language
+        self.seniority = seniority
 
+    def get_annual_salary(self):
+        if self.seniority == 'senior':
+            return (self.base_salary * 12) * 1.3
+        elif self.seniority == 'mid':
+            return (self.base_salary * 12) * 1.15
+        elif self.seniority == 'junior':
+            return (self.base_salary * 12)
+        else:
+            raise ValueError('Invalid seniority level')
+
+    def introduce(self):
+        print(f'Welcome {self.name}. Your ID is: {self.employee_id} and your annual salary is: {self.get_annual_salary()}. You program in {self.programming_language}, and your employee status is: {self.seniority}')
+
+    def code(self, task):
+        print(f'{self.name} is about to finish {task}')
+
+employee1 = Employee('James', 12345, 4000)
+employee2 = Employee('Karin', 21345, 3800)
+employee3 = Manager('Thomas', 54897, 5000, 'hr', 25)
+employee4 = Manager('David', 67890, 6800, 'legal', 12)
+employee5 = Developer('Jim', 54734, 5200, 'JavaScript', 'mid')
+employee6 = Developer('Ben', 98765, 6000, 'Python', 'junior')
+
+print('=' * 25)
 print(employee1)
-print(employee1.get_annual_salary())
 employee1.introduce()
+print('=' * 25)
+print(employee2)
+employee2.introduce()
+print('=' * 25)
+print(employee3)
+employee3.introduce()
+print('=' * 25)
+print(employee4)
+employee4.introduce()
+print('=' * 25)
+print(employee5)
+employee5.introduce()
+print('=' * 25)
+print(employee6)
+employee6.introduce()
+print('=' * 25)
